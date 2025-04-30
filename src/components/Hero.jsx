031820e183d4570f17d0b243a8b87708e5049c82
[aateka limbaliya] <[aatekalimbaliya910@gmail.com]>
2025-05-01 00:35:41 +0530

    Fix vite config and update base path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
         <div className="text-center max-w-3xl mx-auto">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mb-4 sm:mb-6 text-purple-800">Experience the Magic of Indian Weddings</h1>
           <p className="text-base sm:text-lg md:text-xl mb-6 sm:mb-8 px-2 text-pink-800">
-            Immerse yourself in the vibrant colors, rich traditions, and joyful celebrations of authentic Indian weddings as a special guest.
+          "Witness the vows, feel the soul of India."
           </p>
           <Link 
             to="/wedding-types" 
